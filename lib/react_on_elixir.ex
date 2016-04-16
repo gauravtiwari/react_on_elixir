c30baa509e456833dcfb57404dca7d62e24dd3bd
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-16 21:20:35 +0200

    Move supervisor code to separate file

--- lib/react_on_elixir.ex
+++ lib/react_on_elixir.ex
@@ -1,13 +1,7 @@
 defmodule ReactOnElixir do
   use Application
+
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      worker(ReactOnElixir.Worker, [arg1, arg2, arg3]),
-    ]
-
-    opts = [strategy: :one_for_one, name: ReactOnElixir.Supervisor]
-    Supervisor.start_link(children, opts)
+    ReactOnElixir.Supervisor.start_link
   end
 end
