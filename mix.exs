4820fd94c273a00bb10f5b358ef1aa29be62155b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-17 14:40:48 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -5,19 +5,21 @@ defmodule ReactOnElixir.Mixfile do
     [app: :react_on_elixir,
      version: "0.0.1",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     build_embedded: (Mix.env == :prod),
+     start_permanent: (Mix.env == :prod),
      deps: deps]
   end
 
   def application do
-    [applications: [:logger]]
+    [ mod: {ReactOnElixir, []},
+      applications: [:logger, :poolboy, :porcelain]
+    ]
   end
 
   defp deps do
     [
-      {:execjs, "~> 1.1.3"},
-      {:poolboy, "~> 1.5"}
+      {:poolboy, "~> 1.5"},
+      {:poison, "~> 2.0"}
     ]
   end
 end
