3aabecc1fed3b2937b77479dc64ba2f72ccdc8d4
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-17 14:40:33 +0200

    Update supervisor

--- lib/react_on_elixir.ex
+++ lib/react_on_elixir.ex
@@ -1,7 +1,17 @@
 defmodule ReactOnElixir do
   use Application
+  require EEx
 
   def start(_type, _args) do
     ReactOnElixir.Supervisor.start_link
   end
+
+  def renderComponent(options) when is_list(options) do
+    :poolboy.transaction(
+      ReactOnElixir.Config.pool_name(),
+      fn(pid) -> :gen_server.call(pid, options) end,
+      2000
+    )
+  end
 end
+
