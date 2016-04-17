2c1b31aa392d72fc6c1e57a17378d1d6af758d3e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-17 15:27:58 +0200

    Fix paths

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,5 @@
 use Mix.Config
 config :react_on_elixir,
-	server_bundle_path: Mix.Project.app_path <> "/priv/static/js/app-server.js",
-	renderer_path: Mix.Project.app_path <> "/priv/renderer.js.eex",
   pool: [
   	{:name, :react_render},
   	{:type, :local},
